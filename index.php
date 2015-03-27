de64f815ce0aaefb4986629c587552b5761cf4de
Nasc <brase.yns@gmail.com>
2015-03-27 21:32:10 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<title>Dofus Quests</title>
+			<meta charset="utf-8" />
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+		<body>
+			<nav id="toolbar">
+				<ul>
+					<li>Home</li>
+					<li>Quête du DDG</li>
+					<li>Quête du Dofus Emeraude</li>
+					<li>Quête du Dofus Pourpre</li>
+				</ul>
+			</nav>
+			<section>
+				<div id="displayer">
+					<img src="ddg.png" alt="Quêtes du Dofus des Glaces" />
+					<img src="eme.png" alt="Quêtes du Dofus Emeraude" />
+					<img src="prp.png" alt="Quêtes du Dofus Pourpre" />
+				</div>
+			</section>
+			<footer>
+				<p>Toutes les images du site sont la propriété d'Ankama Games.</p>
+			</footer>
+		</body>
+</html>
